4d630fa599ea067666a12a238459643eaac755f3
Parm Grewal <parmgrewal@parms-air.lan>
2023-06-26 15:45:47 -0500

    Add cloudfront distribution. Still need to add CF cache invalidation

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,4 +1,10 @@
-import { aws_iam as iam, aws_s3 as s3, Stack, StackProps } from "aws-cdk-lib";
+import {
+  aws_iam as iam,
+  aws_s3 as s3,
+  aws_cloudfront as cloudfront,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -8,17 +14,27 @@ export class CdkStack extends Stack {
 
     const bucket = new s3.Bucket(this, "SmartSTEPWeb", {
       bucketName: "smartstep-web-first", // Choose your own bucket name here
-      websiteIndexDocument: "index.html", // 1
-      blockPublicAccess: new s3.BlockPublicAccess({
-        restrictPublicBuckets: false,
-      }), // 2
+      websiteIndexDocument: "index.html",
     });
 
-    const bucketPolicy = new iam.PolicyStatement({
-      actions: ["s3:GetObject"],
-      resources: [`${bucket.bucketArn}/*`],
-      principals: [new iam.AnyPrincipal()],
-    });
-    bucket.addToResourcePolicy(bucketPolicy); // 4
+    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, "OAI");
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "MyDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: bucket,
+              originAccessIdentity: cloudFrontOAI,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      }
+    );
+
+    bucket.grantRead(cloudFrontOAI.grantPrincipal);
   }
 }
