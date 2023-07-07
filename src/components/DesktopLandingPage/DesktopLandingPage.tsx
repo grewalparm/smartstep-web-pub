5ffdca3354f4b7c0b72c6bfadf87b68049fa1ffd
Parm Grewal <parmgrewal@parms-air.lan>
2023-07-06 19:19:44 -0500

    Attempt to make DesktopLandingPage more responsive to larger screens

--- src/components/DesktopLandingPage/DesktopLandingPage.tsx
+++ src/components/DesktopLandingPage/DesktopLandingPage.tsx
@@ -4,21 +4,22 @@ import { ReactComponent as Logo } from "../../images/logo.svg";
 
 const DesktopLandingPage = () => {
   return (
-    <div className="flex h-full bg-blue-200 p-[5rem]">
+    <div className="flex h-screen bg-blue-200 p-[5rem]">
       <div className="grid grid-cols-2">
         <div className="flex flex-col rounded-l-[2rem] bg-white pr-32 px-20">
-          <div className="py-20">
+          <div className="flex flex-row items-center text-xl py-20">
             <Logo className="h-14 w-14" />
+            <h1>SmartStepAI</h1>
           </div>
-          <div className="grid grid-cols-1 gap-20">
-            <h1 className="text-6xl font-extrabold">
+          <div className="grid grid-cols-1">
+            <h1 className="flex py-20 text-6xl font-extrabold">
               An AI Tutor for the STEP Exam
             </h1>
-            <p className="text-slate-500">
+            <p className="flex-grow py-15 text-slate-500">
               Prepare for STEP exam success with an AI-powered study partner,
               providing personalized guidance and comprehensive support.
             </p>
-            <div className="pt-2 pb-10">
+            <div className="py-20 pb-10">
               <Link
                 to="/demo"
                 className="rounded-full bg-green-600 text-white px-6 py-3 shadow-lg shadow-green-500 w-40"
@@ -28,10 +29,10 @@ const DesktopLandingPage = () => {
             </div>
           </div>
         </div>
-        <div className="p-3 bg-white rounded-r-[2rem]">
-          <div className="relative">
-            <div className="flex justify-center items-center h-full bg-blue-200 rounded-[2rem] p-10">
-              <Logo className="" />
+        <div className="flex-grow p-3 bg-white rounded-r-[2rem]">
+          <div className="relative h-full">
+            <div className="flex h-full justify-center items-center bg-blue-200 rounded-[2rem] p-10">
+              <Logo className="flex-grow" />
             </div>
             <div className="absolute top-0 right-0 p-6 pb-8 bg-white rounded-bl-[2rem]">
               <Link
